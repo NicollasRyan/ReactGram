172af0cd422856b84579e55dd49d13897824ca37
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-03-31 04:38:27 -0300

    Service para inserir fotos

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -19,3 +19,6 @@ export const photoSlice = createSlice({
     },
   },
 });
+
+export const { restMessage } = photoSlice.actions;
+export default photoSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/authSlices";
 import userReducer from "./slices/userSlice";
+import photoReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    photo: photoReducer,
   },
 });
