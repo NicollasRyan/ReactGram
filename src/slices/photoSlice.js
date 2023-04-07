d19d8053fbe0a36fe9b534556fb996e83fd8b079
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-07 11:36:06 -0300

    Slice de edição de fotos

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -51,6 +51,25 @@ export const deletePhoto = createAsyncThunk(
   }
 );
 
+export const updatePhoto = createAsyncThunk(
+  "photo/update",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.updatePhoto(
+      { title: photoData.title },
+      photoData.id,
+      token
+    );
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -97,12 +116,39 @@ export const photoSlice = createSlice({
         state.loading = false;
         state.success = true;
         state.error = null;
+
         state.photos = state.photos.filter((photo) => {
           return photo._id !== action.payload.id;
         });
+
         state.message = action.payload.message;
       })
       .addCase(deletePhoto.rejected, (state, action) => {
+        console.log(state, action);
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
+      .addCase(updatePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(updatePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photos.map((photo) => {
+          if (photo._id === action.payload.photo._id) {
+            return (photo.title = action.payload.photo.title);
+          }
+
+          return photo;
+        });
+
+        state.message = action.payload.message;
+      })
+      .addCase(updatePhoto.rejected, (state, action) => {
         console.log(state, action);
         state.loading = false;
         state.error = action.payload;
