f3d65ab8f0cd024d43955b101a3dc03f4ea777b6
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-10 05:03:33 -0300

    Exibindo imagem e dados

--- src/pages/Photo/index.js
+++ src/pages/Photo/index.js
@@ -6,7 +6,30 @@ import Message from "../../components/Message";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getPhoto } from "../../slices/photoSlice";
+import { PhotoItem } from "../../components/photoItem";
 
 export function Photo() {
-  return <div></div>;
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { photo, loading, error, message } = useSelector(
+    (state) => state.photo
+  );
+
+  useEffect(() => {
+    dispatch(getPhoto(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="photo">
+      <PhotoItem photo={photo} />
+    </div>
+  );
 }
