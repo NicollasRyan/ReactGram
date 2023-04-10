7e78ae63566cb3af8fc2405bf649ac46728d74d6
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-10 04:48:08 -0300

    Slice da foto por id

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -70,6 +70,12 @@ export const updatePhoto = createAsyncThunk(
   }
 );
 
+export const getPhoto = createAsyncThunk("photo/getphoto", async (id) => {
+  const data = await photoService.getPhoto(id);
+
+  return data;
+});
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -153,6 +159,16 @@ export const photoSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
         state.photo = {};
+      })
+      .addCase(getPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
       });
   },
 });
