e2fc604049695dd64fba4f15c4de5e2e3cd6b1c1
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-11 12:38:30 -0300

    Service de busca

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,11 +3,12 @@ import "./style.css";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
+import { useQuery } from "../../hooks/useQuery";
 
 import { LikeContainer } from "../../components/LikeContainer";
 import { PhotoItem } from "../../components/photoItem";
 import { Link } from "react-router-dom";
 
 export function Search() {
-  return <div></div>;
+  return <div>Messi é o caralho</div>;
 }
