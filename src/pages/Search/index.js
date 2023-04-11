18b3ecaed22ceb6b1ce58c386b588dd80d6d655c
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-11 05:26:00 -0300

    Criando página de busca

---
+++ src/pages/Search/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
+
+import { LikeContainer } from "../../components/LikeContainer";
+import { PhotoItem } from "../../components/photoItem";
+import { Link } from "react-router-dom";
+
+export function Search() {
+  return <div></div>;
+}
