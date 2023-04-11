5f956f5c3b1775287d37932b8479259f20ffc525
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-04-11 13:07:51 -0300

    Slice de busca

--- src/slices/photoSlice.js
+++ src/slices/photoSlice.js
@@ -123,6 +123,17 @@ export const getPhotos = createAsyncThunk(
   }
 );
 
+export const searchPhotos = createAsyncThunk(
+  "photo/search",
+  async (query, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.searchPhotos(query, token);
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -258,6 +269,16 @@ export const photoSlice = createSlice({
         state.error = null;
       })
       .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(searchPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(searchPhotos.fulfilled, (state, action) => {
         state.loading = false;
         state.success = true;
         state.error = null;
